5b90f88e1183b10983a97c3d9f46b7a12e23f9d0
nizam <ishanov@smartgeosystems.ru>
2024-02-24 12:36:01 +0500

    fix

--- src/main/java/Usersdbgradle/repo/UsersDBRepo.java
+++ src/main/java/Usersdbgradle/repo/UsersDBRepo.java
@@ -2,7 +2,6 @@ package Usersdbgradle.repo;
 
 import Usersdbgradle.models.UsersDB;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.scheduling.config.Task;
 
 public interface UsersDBRepo extends CrudRepository<UsersDB, Long> {
 
