4784fe7d82f68ae91cb60b108d67f8d760e33498
nizam <ishanov@smartgeosystems.ru>
2024-03-07 21:59:34 +0500

    junit first test

--- src/main/java/Usersdbgradle/service/UserService.java
+++ src/main/java/Usersdbgradle/service/UserService.java
@@ -9,14 +9,24 @@ import java.util.List;
 
 @Service
 public class UserService {
+
     @Autowired
     private UsersDBRepo usersRepo;
+
     public List<UsersDB3> getDetails() {
         return usersRepo.findAll();
+//        throw new RuntimeException("error");
     }
-    public UsersDB3 saveDetails(UsersDB3 user){ return usersRepo.save(user); }
-    public List<UsersDB3> saveListDetails(List<UsersDB3> users){ return usersRepo.saveAll(users); }
-    public String deleteStudent(long id){
+
+    public UsersDB3 saveDetails(UsersDB3 user) {
+        return usersRepo.save(user);
+    }
+
+    public List<UsersDB3> saveListDetails(List<UsersDB3> users) {
+        return usersRepo.saveAll(users);
+    }
+
+    public String deleteStudent(long id) {
         if (usersRepo.existsById(id)) {
             usersRepo.deleteById(id);
             return "deleted" + id;
@@ -24,9 +34,10 @@ public class UserService {
             return "ID is not existing";
         }
     }
-    public UsersDB3 updateDetails(UsersDB3 user){
+
+    public UsersDB3 updateDetails(UsersDB3 user) {
         UsersDB3 updateUser = usersRepo.findById(user.getId()).orElse(null);
-        if(updateUser!=null){
+        if (updateUser != null) {
             updateUser.setEmail(user.getEmail());
             updateUser.setFullName(user.getFullName());
             updateUser.setPassword(user.getPassword());
@@ -35,7 +46,8 @@ public class UserService {
         }
         return null;
     }
-    public UsersDB3 getUserDetailsById(long id){
+
+    public UsersDB3 getUserDetailsById(long id) {
         return usersRepo.findById(id).orElse(null);
 
     }
--- src/test/java/Usersdbgradle/service/UserServiceTest.java
+++ src/test/java/Usersdbgradle/service/UserServiceTest.java
@@ -2,35 +2,41 @@ package Usersdbgradle.service;
 
 import Usersdbgradle.models.UsersDB3;
 import Usersdbgradle.repo.UsersDBRepo;
-import Usersdbgradle.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class UserServiceTest {
-    private UsersDBRepo usersRepo = mock(UsersDBRepo.class);
-    private UserService userService = mock(UserService.class);
 
-    private MockMvc mockMvc;
+    private final UsersDBRepo usersRepo = mock(UsersDBRepo.class);
+    private final UserService userService = mock(UserService.class);
+//    private MockMvc mockMvc;
 
     @Test
     void getDetails() {
+
         UsersDB3 user1 = new UsersDB3("FullName", "email@email.org", "123123");
         UsersDB3 user2 = new UsersDB3("FullName2", "fullname@email.org", "34563456");
+        List<UsersDB3> actualUsers = Arrays.asList(user1, user2);
         List<UsersDB3> expectedUsers = Arrays.asList(user1, user2);
-        when(usersRepo.findAll()).thenReturn(expectedUsers);
-        List<UsersDB3> actualUsers = userService.getDetails();
-        verify(usersRepo).findAll();
-        assertEquals(expectedUsers, actualUsers);
+
+        when(usersRepo.findAll()).thenReturn(actualUsers);
+
+        when(userService.getDetails()).thenReturn(expectedUsers);
+
+        assertThat(expectedUsers).isNotNull();
+
+        userService.getDetails();
+        verify(userService, times(1)).getDetails();
+
+        assertEquals(actualUsers, expectedUsers);
+
     }
+
+
 }
